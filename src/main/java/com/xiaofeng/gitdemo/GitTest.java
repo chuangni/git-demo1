ad77fa7b1ac25364eca705ce0744970c50045d6a
chuanga <yuchuang_feng@163.com>
2023-02-03 13:26:19 +0800

    hot-fix commit2

--- src/main/java/com/xiaofeng/gitdemo/GitTest.java
+++ src/main/java/com/xiaofeng/gitdemo/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git2 hot-fix test");
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git5");
