a79811892f5c3a40289de449c88bc931532fa633
chuanga <yuchuang_feng@163.com>
2023-02-03 13:28:07 +0800

    hot-fix commit3

--- src/main/java/com/xiaofeng/gitdemo/GitTest.java
+++ src/main/java/com/xiaofeng/gitdemo/GitTest.java
@@ -12,6 +12,6 @@ public class GitTest {
         System.out.println("hello git2 hot-fix test");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("hello git5");
+
     }
 }
